8dd7bdb4b6857f0b4549ee29a8452e961ed638fd
Eszti <117855116+r1eszter@users.noreply.github.com>
2023-12-01 22:58:16 +0100

    Add files via upload

--- XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomQueryQTFL19.java
+++ XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomQueryQTFL19.java
@@ -1,199 +1,117 @@
 package hu.domparse.QTFL19;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 
 public class DomQueryQTFL19 {
 
 	public static void main(String[] args) {
-        
+
+        List<String> kb_lista = new ArrayList<>(); /* könyvesbolt lista */
+        List<String> k_lista = new ArrayList<>(); /* könyv lista */
+        List<String> v_lista = new ArrayList<>(); /* vasarlo lista */
+        List<String> ki_lista = new ArrayList<>(); /* kiado lista */
+        List<String> kar_lista = new ArrayList<>(); /* kiado lista */
+
         try{
             File xmlFile = new File("XMLQTFL19.xml");
 
             /* Dokumentum Builder létrekozása */
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = factory.newDocumentBuilder();
-
             Document doc = dBuilder.parse(xmlFile);
-
             doc.getDocumentElement().normalize();
 
-            /* xPath készítése */
-			XPath xPath = XPathFactory.newInstance().newXPath();
+            /* 1. Lekérdezi a könyvesboltok nevét és kapacitását azoknak ahol az email címében található gmail rész */
+            NodeList kbNodeList = doc.getElementsByTagName("konyvesbolt");
+            System.out.print("\n1. Könyvesboltok gmail-el lekérdezés");
 
-            /* Lekérdezések */
-            /* 1. Lekérdezi a root (konyvesvasarlas) elem konyvesbolt gyerekelemeit */
-            //String expression = "konyvesvasarlas / konyvesbolt";
+            for (int i=0; i<kbNodeList.getLength(); i++) {
+                Element kbElement = (Element) kbNodeList.item(i);
+                String kb = kbElement.getTextContent();
+                kb_lista.add(kb);
 
-            /* 2. Lekérdezi a harmadik vasarlot */
-            //String expression = "konyvesvasarlas / vasarlo[3]";
+                String kb_email = kbElement.getElementsByTagName("email").item(0).getTextContent();
 
-            /* 3. Lekérdezi azokat a könyveket amiknek a mufajai között található thriller */
-            //String expression = "//konyv[mufaj='thriller']";
-
-            /* 4. Lekérdezi a vasarlo első két elemét */
-            //String expression = "//vasarlo[position()<3]";
-
-            /* 5. Lekérdezi a 85632982 id-val rendelkező kártyát */
-            //String expression = "//kartya[@vonalkod='85632982']";
-
-            /* 6. Lekérdezi azokat a konyvesboltokat ahol a kapacitás több mint 20.000 db */
-            //String expression = "//konyvesbolt[kapacitas>20.000]";
-
-            /* 7. Lekérdezi a kartyakat amik bármilyen tipusuak lehetnek*/
-            //String expression = "//kartya[tipus=*]";
-
-            /* 8. Lekérdezi */
-            //String expression = "//kartya[akcio>30]/tipus";
-
-            /* 9. Lekérdezi az első kettő kivételével az összes kiado nevét */
-            //String expression = "//kiado[position()>2]/nev";
-
-            /* 10. Lekérdezi azon könyvek címét amelyeknek bármilyen attribútuma lehet */
-            String expression = "//konyv[@*]/cim";
-            
-
-            /* Lista ami az xPath kifejezést le fordítja és ki értékeli  */
-			NodeList kList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
-
-            /* Végig megyünk a nodeList csomópontjain */
-            for (int i = 0; i < kList.getLength(); i++) {
-				Node node = kList.item(i);
-
-				System.out.println("\nAktuális elem: " + node.getNodeName());
-
-				// Könyvesbolt csomópont megvizsgálása
-				if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("konyvesbolt")) {
-
-					Element element = (Element) node;
-
-					System.out.println("ID: " + element.getAttribute("KB_ID"));
-					System.out.println("Neve: " + element.getElementsByTagName("nev").item(0).getTextContent());
-
-                    if (kList.item(i).getChildNodes().getLength() > 3) {
-						int db = 0;
-						Node node2 = element.getElementsByTagName("elerhetoseg").item(0);
-						while (node2 != null) {
-							node2 = element.getElementsByTagName("elerhetoseg").item(db);
-							if (node2 != null) {
-								Node n = element.getElementsByTagName("email").item(db);
-								String isz = n.getTextContent();
-								System.out.println("Email: " + isz);
-								Node n2 = element.getElementsByTagName("telefon").item(db);
-								String u = n2.getTextContent();
-								System.out.println("Telefon szam: " + u);
-							}
-							db++;
-						}
-					}
-
-					System.out.println("Kapacitas: " + element.getElementsByTagName("kapacitas").item(0).getTextContent());
-				}
-
-
-                /* Könyv */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("konyv")) {
-
-					Element element = (Element) node;
-
-					System.out.println("ID: " + element.getAttribute("K_ID"));
-					System.out.println("Címe: " + element.getElementsByTagName("cim").item(0).getTextContent());
-
-					if (kList.item(i).getChildNodes().getLength() > 5) {
-						int db = 0;
-						Node node2 = element.getElementsByTagName("mufaj").item(0);
-						while (node2 != null) {
-							node2 = element.getElementsByTagName("mufaj").item(db);
-							if (node2 != null) {
-								String rh = node2.getTextContent();
-								System.out.println("Mufaj: " + rh);
-							}
-							db++;
-						}
-					}
-                    					
-                    System.out.println("Író: " + element.getElementsByTagName("iro").item(0).getTextContent());
+                if (kb_email.matches(".+@gmail.+")){
+                    System.out.print("\n" + kbElement.getElementsByTagName("nev").item(0).getTextContent() + " : " + kbElement.getElementsByTagName("kapacitas").item(0).getTextContent() + "db");
                 }
-
-                /* Könyv címe */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("cim")) {
-                    Element element = (Element) node;
-                    System.out.println("Könyv címe: " + element.getTextContent());
-                }
-
-
-                /* Kiadó */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("kiado")) {
-
-					Element element = (Element) node;
-
-					System.out.println("Adószám: " + element.getAttribute("Adoszam"));
-					System.out.println("Neve: " + element.getElementsByTagName("nev").item(0).getTextContent());    
-					System.out.println("Alapítása: " + element.getElementsByTagName("alapitas").item(0).getTextContent());    
-					System.out.println("Helye: " + element.getElementsByTagName("hely").item(0).getTextContent());  
-                }  
-
-                /* Kiadó neve */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("nev")) {
-                    Element element = (Element) node;
-                    System.out.println("Kiadó neve: " + element.getTextContent());
-                }
-
-                /* Vásárló */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("vasarlo")) {
-
-					Element element = (Element) node;
-
-					System.out.println("Adószám: " + element.getAttribute("V_ID"));
-					System.out.println("Neve: " + element.getElementsByTagName("nev").item(0).getTextContent());    
-					System.out.println("Irányítószáma: " + element.getElementsByTagName("iranyitoszam").item(0).getTextContent());    
-					System.out.println("E-mail címe: " + element.getElementsByTagName("email").item(0).getTextContent());  
-                }  
-
-
-                /* Kártya */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("kartya")) {
-
-					Element element = (Element) node;
-
-					System.out.println("Vonalkód: " + element.getAttribute("vonalkod"));
-					System.out.println("Tipusa: " + element.getElementsByTagName("tipus").item(0).getTextContent());    
-					System.out.println("Akció értéke: " + element.getElementsByTagName("akcio").item(0).getTextContent());    
-					System.out.println("Igénylés dátuma: " + element.getElementsByTagName("igenyles").item(0).getTextContent());  
-                }  
-
-                /* Kártya tipus */
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("tipus")) {
-                    Element element = (Element) node;
-                    System.out.println("Kártya tipusa: " + element.getTextContent());
-                }
-
             }
 
-        } catch (ParserConfigurationException e) {
-        e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XPathExpressionException e) {
+            /* 2. Lekérdezi az összes könyv címét és íróját */
+            NodeList kNodeList = doc.getElementsByTagName("konyv");
+            System.out.print("\n\n\n2. Összes könyv lekérdezése (cím, író)");
+
+            for (int i=0; i<kNodeList.getLength(); i++) {
+                Element kElement = (Element) kNodeList.item(i);
+                String k_sum = kElement.getTextContent();
+                k_lista.add(k_sum);
+                
+                System.out.print("\nKönyv címe : " + kElement.getElementsByTagName("cim").item(0).getTextContent());
+                System.out.print("\t írója : " + kElement.getElementsByTagName("iro").item(0).getTextContent());
+            }
+
+            /* 3. Lekérdezi egy konkrét vásárló adatait (Súkeník Erik) */
+            NodeList vNodeList = doc.getElementsByTagName("vasarlo");
+            System.out.print("\n\n\n3. Súkeník Erik adatainak lekérdezése");
+
+            for (int i=0; i<vNodeList.getLength(); i++) {
+                Element vElement = (Element) vNodeList.item(i);
+                String v_sum = vElement.getTextContent();
+                v_lista.add(v_sum);
+
+                String v_name = vElement.getElementsByTagName("nev").item(0).getTextContent();
+                
+                if (v_name.equals("Súkeník Erik")) {
+                    System.out.print("\nID : " + vElement.getAttribute("V_ID") + "\nIrányítószáma : " + vElement.getElementsByTagName("iranyitoszam").item(0).getTextContent() + "\nEmail címe : " + vElement.getElementsByTagName("email").item(0).getTextContent());
+                }
+            }
+
+            /* 4. Lekérdezi az összes kiadó nevét, alapítási dátumát és helyét ahol az alapítás 2000 előtt történt */
+            NodeList kiNodeList = doc.getElementsByTagName("kiado");
+            System.out.print("\n\n\n4. Kiadó alapítás lekérdezés");
+
+            for (int i=0; i<kiNodeList.getLength(); i++) {
+                Element kiElement = (Element) kiNodeList.item(i);
+                String kiado = kiElement.getTextContent();
+                ki_lista.add(kiado);
+
+                String ki_alap = kiElement.getElementsByTagName("alapitas").item(0).getTextContent();
+
+                if (ki_alap.startsWith("19")) {
+                    System.out.print("\nNeve : " + kiElement.getElementsByTagName("nev").item(0).getTextContent() + "\nAlapítása : " + kiElement.getElementsByTagName("alapitas").item(0).getTextContent() + "\nHelye : " + kiElement.getElementsByTagName("hely").item(0).getTextContent() + "\n");
+                }
+            }
+
+            /* 5. Lekérdezi az összes kártyát amelyiknek a kulcsában (vonalkódjában) található kettes */
+            NodeList karNodeList = doc.getElementsByTagName("kartya");
+            System.out.print("\n\n\n5. Kártya vonalkód lekérdezés");
+
+            for (int i=0; i<karNodeList.getLength(); i++) {
+                Element karElement = (Element) karNodeList.item(i);
+                String kartya = karElement.getTextContent();
+                kar_lista.add(kartya);
+
+                String kar_id = karElement.getAttribute("vonalkod");
+
+                if (kar_id.matches(".+2.+")){
+                    System.out.print("\nTipus : " + karElement.getElementsByTagName("tipus").item(0).getTextContent() + "\nAkció : " + karElement.getElementsByTagName("akcio").item(0).getTextContent() + "\n");
+                }
+            }
+
+        }catch (Exception e) {
             e.printStackTrace();
         }
+
 
     }
 }
