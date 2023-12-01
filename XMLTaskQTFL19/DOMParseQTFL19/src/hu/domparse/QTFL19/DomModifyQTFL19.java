6a1727f9085a7f91f41f93cc5d623711ac3cfedf
Eszti <117855116+r1eszter@users.noreply.github.com>
2023-12-02 00:12:25 +0100

    Add files via upload

--- XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomModifyQTFL19.java
+++ XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomModifyQTFL19.java
@@ -1,73 +1,86 @@
-package domQTFL191115;
+package hu.domparse.QTFL19;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-
 public class DomModifyQTFL19 {
 
     public static void main(String argv[]) {
 
-		try {
-			File inputFile = new File("kurzusfelvetelQTFL19.xml");
-            
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        try {
+            File xmlFile = new File("XMLQTFL19.xml");
 
-            Document doc = documentBuilder.parse(inputFile);
+            /* Dokumentum Builder létrekozása */
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+			doc.getDocumentElement().normalize();
 
-            doc.getDocumentElement().normalize();
-            NodeList kurzusNodeList = doc.getElementsByTagName("kurzus");
-
-            for (int i=0; i<kurzusNodeList.getLength(); i++) {
-                Element kurzusNode = (Element) kurzusNodeList.item(i);
-                Element oraadoNode = (Element) kurzusNode.getElementsByTagName("oraado").item(0);
-                String nyelvAttr = kurzusNode.getAttribute("nyelv");
-
-                if (oraadoNode == null) {
-                    oraadoNode = doc.createElement("oraado");
-                    oraadoNode.appendChild(doc.createTextNode("Agárdi Anita"));
-                    kurzusNode.appendChild(oraadoNode);
-                }
-
-                if (nyelvAttr.equals("angol")) {
-                    kurzusNode.setAttribute("nyelv", "német");
-                }
+			
+			/* 1. Könyvesbolt kapacitásának cseréja HA az attribútuma kb1 */
+            NodeList kbNodeList = doc.getElementsByTagName("konyvesbolt");
+			for (int i=0; i<kbNodeList.getLength(); i++) {
+                Element kbNode = (Element) kbNodeList.item(i);
+				String kbAttr = kbNode.getAttribute("KB_ID");
+				
+				if (kbAttr.equals("kb1")) {
+					kbNode.getElementsByTagName("kapacitas").item(0).setTextContent("20.400");
+					System.out.print("1. Módosított kapacitás : " + kbNode.getElementsByTagName("kapacitas").item(0).getTextContent());
+				}
             }
 
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            DOMSource source = new DOMSource(doc);
-            StreamResult consoleResult = new StreamResult(System.out);
-            transformer.transform(source, consoleResult);
+			/* 2. Könyv  */
+			NodeList kiNodeList = doc.getElementsByTagName("kiado");
+			for(int i=0; i<kiNodeList.getLength(); i++) {
+				Element kiNode = (Element) kiNodeList.item(i);
+				String ki_hely = kiNode.getElementsByTagName("hely").item(0).getTextContent();
 
-            File outputFile = new File("kurzusfelvetelModify1QTFL19.xml");
-            OutputStream outputStream = new FileOutputStream(outputFile);
-            StreamResult fileResult = new StreamResult(outputStream);
-            transformer.transform(source, fileResult);
-            outputStream.close();
+				if (ki_hely.length() < 10) {
+					kiNode.getElementsByTagName("hely").item(0).setTextContent(ki_hely + " (új hely szükséges)");
+					System.out.print("\n\n2. Új hely:" + kiNode.getElementsByTagName("hely").item(0).getTextContent());
+				}	
+			}
 
-        } catch (ParserConfigurationException | IOException | TransformerException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			/* 3. Megváltoztatja az első könyv címét */
+			NodeList kNodeList = doc.getElementsByTagName("konyv");
+			Element kNode = (Element) kNodeList.item(0);
+			kNode.getElementsByTagName("cim").item(0).setTextContent("League of Legends");
+			System.out.print("\n\n3. Új cím:" + kNode.getElementsByTagName("cim").item(0).getTextContent());
+
+			/* 4. A vásárlóknak akinek van á a nevükben meg változtatja a nevüket + szöveggel*/
+			NodeList vNodeList = doc.getElementsByTagName("vasarlo");
+			for(int i=0; i<vNodeList.getLength(); i++) {
+				Element vNode = (Element) vNodeList.item(i);
+				String v_nev = vNode.getElementsByTagName("nev").item(0).getTextContent();
+
+				if (v_nev.matches(".+á.+")) {
+					vNode.getElementsByTagName("nev").item(0).setTextContent(v_nev + " van egy á a nevemben");
+					System.out.print("\n\n4. Van á?:" + vNode.getElementsByTagName("nev").item(0).getTextContent());
+				}	
+			}
+
+			/* 5. Az első három kártya kivételével mindnek meg cseréli a tipusát Ajándékra  */
+			NodeList kaNodeList = doc.getElementsByTagName("kartya");
+			for(int i=0; i<kaNodeList.getLength(); i++) {
+				Element kaNode = (Element) kaNodeList.item(i);
+				
+				if (i > 2) {
+					kaNode.getElementsByTagName("tipus").item(0).setTextContent("Ajandek");
+					System.out.print("\n\n5. Új tipus: " + kaNode.getElementsByTagName("tipus").item(0).getTextContent());
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        
+
     }
 }
