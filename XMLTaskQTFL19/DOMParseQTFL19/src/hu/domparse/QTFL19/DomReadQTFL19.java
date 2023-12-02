26479ef403a5b1898d92f98df4601cf7967937a8
Eszti <117855116+r1eszter@users.noreply.github.com>
2023-12-03 00:14:00 +0100

    Add files via upload

--- XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomReadQTFL19.java
+++ XMLTaskQTFL19/DOMParseQTFL19/src/hu/domparse/QTFL19/DomReadQTFL19.java
@@ -1,11 +1,19 @@
 package hu.domparse.QTFL19;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -17,217 +25,238 @@ public class DomReadQTFL19 {
     
     public static void main(String argv[]) throws SAXException, IOException, ParserConfigurationException {
         
-        File xmlFile = new File("XMLQTFL19.xml");
-        
-        /* Dokumentum Builder létrekozása */
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = factory.newDocumentBuilder();
+		try {
 
-        Document doc = dBuilder.parse(xmlFile);
-
-		doc.getDocumentElement().normalize();
-
-		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		System.out.println("\n<" + doc.getDocumentElement().getNodeName() + ">");
 		
+			File xmlFile = new File("XMLQTFL19.xml");
+			
+			/* Dokumentum Builder létrekozása */
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
+			doc.getDocumentElement().normalize();
 
-        /* A könyvesbolt beolvasása */
-        NodeList kbList = doc.getElementsByTagName("konyvesbolt");
-        for (int i = 0; i < kbList.getLength(); i++) {
+			System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			System.out.println("\n<" + doc.getDocumentElement().getNodeName() + ">");
+			
 
-			Node nNode = kbList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			/* A könyvesbolt beolvasása */
+			NodeList kbList = doc.getElementsByTagName("konyvesbolt");
+			for (int i = 0; i < kbList.getLength(); i++) {
 
-				Element elem = (Element) nNode;
-				String kbid = elem.getAttribute("KB_ID");
+				Node nNode = kbList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-				Node node1 = elem.getElementsByTagName("nev").item(0);
-				String kbnev = node1.getTextContent();
+					Element elem = (Element) nNode;
+					String kbid = elem.getAttribute("KB_ID");
 
-                /* a node2 lesz az elérhetőség */
+					Node node1 = elem.getElementsByTagName("nev").item(0);
+					String kbnev = node1.getTextContent();
 
-				Node node3 = elem.getElementsByTagName("kapacitas").item(0);
-				String kap = node3.getTextContent();
+					/* a node2 lesz az elérhetőség */
 
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " KB_ID=" + kbid + ">");
-				System.out.println("\t  <nev>" + kbnev + "</nev>");
-                
-                if (kbList.item(i).getChildNodes().getLength() > 3) {
-					int db = 0;
-					Node node2 = elem.getElementsByTagName("elerhetoseg").item(0);
-					while (node2 != null) {
-						node2 = elem.getElementsByTagName("elerhetoseg").item(db);
-						if (node2 != null) {
-							Node n = elem.getElementsByTagName("email").item(db);
-							String em = n.getTextContent();
-							System.out.println("\t    <email>" + em + "</email>");
+					Node node3 = elem.getElementsByTagName("kapacitas").item(0);
+					String kap = node3.getTextContent();
 
-							Node n2 = elem.getElementsByTagName("telefon").item(db);
-							String tel = n2.getTextContent();
-							System.out.println("\t    <telefon>" + tel + "</telefon>");
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " KB_ID=" + kbid + ">");
+					System.out.println("\t  <nev>" + kbnev + "</nev>");
+					
+					if (kbList.item(i).getChildNodes().getLength() > 3) {
+						int db = 0;
+						Node node2 = elem.getElementsByTagName("elerhetoseg").item(0);
+						while (node2 != null) {
+							node2 = elem.getElementsByTagName("elerhetoseg").item(db);
+							if (node2 != null) {
+								Node n = elem.getElementsByTagName("email").item(db);
+								String em = n.getTextContent();
+								System.out.println("\t    <email>" + em + "</email>");
+
+								Node n2 = elem.getElementsByTagName("telefon").item(db);
+								String tel = n2.getTextContent();
+								System.out.println("\t    <telefon>" + tel + "</telefon>");
+							}
+							db++;
 						}
-						db++;
 					}
+					System.out.println("\t  <kapacitas>" + kap + "</kapacitas>");
 				}
-                System.out.println("\t  <kapacitas>" + kap + "</kapacitas>");
+				System.out.println("\t</" + nNode.getNodeName() + ">");
 			}
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-		}
 
 
 
-        /* A könyv elem beolvasása */
-        NodeList kList = doc.getElementsByTagName("konyv");
-        for (int i = 0; i < kList.getLength(); i++) {
+			/* A könyv elem beolvasása */
+			NodeList kList = doc.getElementsByTagName("konyv");
+			for (int i = 0; i < kList.getLength(); i++) {
 
-			Node nNode = kList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Node nNode = kList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-				Element elem = (Element) nNode;
-				String kid = elem.getAttribute("K_ID");
+					Element elem = (Element) nNode;
+					String kid = elem.getAttribute("K_ID");
 
-				Node node1 = elem.getElementsByTagName("cim").item(0);
-				String cim = node1.getTextContent();
+					Node node1 = elem.getElementsByTagName("cim").item(0);
+					String cim = node1.getTextContent();
 
-                /* A node2 itt a mufaj */
+					/* A node2 itt a mufaj */
 
-                Node node3 = elem.getElementsByTagName("iro").item(0);
-				String iro = node3.getTextContent();
+					Node node3 = elem.getElementsByTagName("iro").item(0);
+					String iro = node3.getTextContent();
 
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " K_ID = " + kid + ">");
-                System.out.println("\t  <cim>" + cim + "</cim>");
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " K_ID = " + kid + ">");
+					System.out.println("\t  <cim>" + cim + "</cim>");
 
-                if (kList.item(i).getChildNodes().getLength() > 5) {
-					int db = 0;
-					Node node2 = elem.getElementsByTagName("mufaj").item(0);
-					while (node2 != null) {
-						node2 = elem.getElementsByTagName("mufaj").item(db);
-						if (node2 != null) {
-							String rh = node2.getTextContent();
-							System.out.println("\t  <mufaj>" + rh + "</mufaj>");
+					if (kList.item(i).getChildNodes().getLength() > 5) {
+						int db = 0;
+						Node node2 = elem.getElementsByTagName("mufaj").item(0);
+						while (node2 != null) {
+							node2 = elem.getElementsByTagName("mufaj").item(db);
+							if (node2 != null) {
+								String rh = node2.getTextContent();
+								System.out.println("\t  <mufaj>" + rh + "</mufaj>");
+							}
+							db++;
 						}
-						db++;
 					}
+					
+					System.out.println("\t  <iro>" + iro + "</iro>");
 				}
-                
-                System.out.println("\t  <iro>" + iro + "</iro>");
-            }
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-        }
-
-
-
-        /* Kiadó elem beolvasása */
-        NodeList kiList = doc.getElementsByTagName("kiado");
-        for (int i = 0; i < kiList.getLength(); i++) {
-
-			Node nNode = kiList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-				Element elem = (Element) nNode;
-				String ado = elem.getAttribute("Adoszam");
-
-				Node node1 = elem.getElementsByTagName("nev").item(0);
-				String kinev = node1.getTextContent();
-
-                Node node2 = elem.getElementsByTagName("alapitas").item(0);
-				String alap = node2.getTextContent();
-
-				Node node3 = elem.getElementsByTagName("hely").item(0);
-				String hely = node3.getTextContent();
-
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " Adoszam = " + ado + ">");
-                System.out.println("\t  <nev>" + kinev + "</nev>");
-				System.out.println("\t  <alapitas>" + alap + "</alapitas>");
-                System.out.println("\t  <hely>" + hely + "</hely>");
-            }
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-        }
-
-
-        /* Vásárló egyed beolvasása */
-        NodeList vList = doc.getElementsByTagName("vasarlo");
-        for (int i = 0; i < vList.getLength(); i++) {
-
-			Node nNode = vList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-				Element elem = (Element) nNode;
-				String vid = elem.getAttribute("V_ID");
-
-				Node node1 = elem.getElementsByTagName("nev").item(0);
-				String vnev = node1.getTextContent();
-
-                Node node2 = elem.getElementsByTagName("iranyitoszam").item(0);
-				String irany = node2.getTextContent();
-
-				Node node3 = elem.getElementsByTagName("email").item(0);
-				String vem = node3.getTextContent();
-
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " V_ID = " + vid + ">");
-                System.out.println("\t  <nev>" + vnev + "</nev>");
-				System.out.println("\t  <iranyitoszam>" + irany + "</iranyitoszam>");
-                System.out.println("\t  <email>" + vem + "</email>");
-            }
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-        }
-
-
-        /* Kártya egyed beolvasása */
-        NodeList kaList = doc.getElementsByTagName("kartya");
-        for (int i = 0; i < kaList.getLength(); i++) {
-
-			Node nNode = kaList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-				Element elem = (Element) nNode;
-				String von = elem.getAttribute("vonalkod");
-
-				Node node1 = elem.getElementsByTagName("tipus").item(0);
-				String tip = node1.getTextContent();
-
-                Node node2 = elem.getElementsByTagName("akcio").item(0);
-				String akcio = node2.getTextContent();
-
-				Node node3 = elem.getElementsByTagName("igenyles").item(0);
-				String ig = node3.getTextContent();
-
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " vonalkod = " + von + ">");
-                System.out.println("\t  <tipus>" + tip + "</tipus>");
-				System.out.println("\t  <akcio>" + akcio + "</akcio>");
-                System.out.println("\t  <igenyles>" + ig + "</igenyles>");
+				System.out.println("\t</" + nNode.getNodeName() + ">");
 			}
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-		}
 
 
-		/* vétel kapcsolat */
-		NodeList vetList = doc.getElementsByTagName("vetel");
-        for (int i = 0; i < vetList.getLength(); i++) {
 
-			Node nNode = vetList.item(i);
-			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			/* Kiadó elem beolvasása */
+			NodeList kiList = doc.getElementsByTagName("kiado");
+			for (int i = 0; i < kiList.getLength(); i++) {
 
-				Element elem = (Element) nNode;
-				String kbid = elem.getAttribute("KB_ID");
-				String vid = elem.getAttribute("V_ID");
+				Node nNode = kiList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-				Node node1 = elem.getElementsByTagName("vasarlas").item(0);
-				String vas = node1.getTextContent();
+					Element elem = (Element) nNode;
+					String ado = elem.getAttribute("Adoszam");
 
-				/* struktúrált ki iratás */
-				System.out.println("\n\t<" + nNode.getNodeName() + " KB_ID = " + kbid + " " + " V_ID = " + vid + ">");
-                System.out.println("\t  <vasarlas> " + vas + "</vasarlas>");
+					Node node1 = elem.getElementsByTagName("nev").item(0);
+					String kinev = node1.getTextContent();
 
+					Node node2 = elem.getElementsByTagName("alapitas").item(0);
+					String alap = node2.getTextContent();
+
+					Node node3 = elem.getElementsByTagName("hely").item(0);
+					String hely = node3.getTextContent();
+
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " Adoszam = " + ado + ">");
+					System.out.println("\t  <nev>" + kinev + "</nev>");
+					System.out.println("\t  <alapitas>" + alap + "</alapitas>");
+					System.out.println("\t  <hely>" + hely + "</hely>");
+				}
+				System.out.println("\t</" + nNode.getNodeName() + ">");
 			}
-			System.out.println("\t</" + nNode.getNodeName() + ">");
-		}
 
-		System.out.println("\n</" + doc.getDocumentElement().getNodeName() + ">");
-    }
+
+			/* Vásárló egyed beolvasása */
+			NodeList vList = doc.getElementsByTagName("vasarlo");
+			for (int i = 0; i < vList.getLength(); i++) {
+
+				Node nNode = vList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element elem = (Element) nNode;
+					String vid = elem.getAttribute("V_ID");
+
+					Node node1 = elem.getElementsByTagName("nev").item(0);
+					String vnev = node1.getTextContent();
+
+					Node node2 = elem.getElementsByTagName("iranyitoszam").item(0);
+					String irany = node2.getTextContent();
+
+					Node node3 = elem.getElementsByTagName("email").item(0);
+					String vem = node3.getTextContent();
+
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " V_ID = " + vid + ">");
+					System.out.println("\t  <nev>" + vnev + "</nev>");
+					System.out.println("\t  <iranyitoszam>" + irany + "</iranyitoszam>");
+					System.out.println("\t  <email>" + vem + "</email>");
+				}
+				System.out.println("\t</" + nNode.getNodeName() + ">");
+			}
+
+
+			/* Kártya egyed beolvasása */
+			NodeList kaList = doc.getElementsByTagName("kartya");
+			for (int i = 0; i < kaList.getLength(); i++) {
+
+				Node nNode = kaList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element elem = (Element) nNode;
+					String von = elem.getAttribute("vonalkod");
+
+					Node node1 = elem.getElementsByTagName("tipus").item(0);
+					String tip = node1.getTextContent();
+
+					Node node2 = elem.getElementsByTagName("akcio").item(0);
+					String akcio = node2.getTextContent();
+
+					Node node3 = elem.getElementsByTagName("igenyles").item(0);
+					String ig = node3.getTextContent();
+
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " vonalkod = " + von + ">");
+					System.out.println("\t  <tipus>" + tip + "</tipus>");
+					System.out.println("\t  <akcio>" + akcio + "</akcio>");
+					System.out.println("\t  <igenyles>" + ig + "</igenyles>");
+				}
+				System.out.println("\t</" + nNode.getNodeName() + ">");
+			}
+
+
+			/* vétel kapcsolat */
+			NodeList vetList = doc.getElementsByTagName("vetel");
+			for (int i = 0; i < vetList.getLength(); i++) {
+
+				Node nNode = vetList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element elem = (Element) nNode;
+					String kbid = elem.getAttribute("KB_ID");
+					String vid = elem.getAttribute("V_ID");
+
+					Node node1 = elem.getElementsByTagName("vasarlas").item(0);
+					String vas = node1.getTextContent();
+
+					/* struktúrált ki iratás */
+					System.out.println("\n\t<" + nNode.getNodeName() + " KB_ID = " + kbid + " " + " V_ID = " + vid + ">");
+					System.out.println("\t  <vasarlas> " + vas + "</vasarlas>");
+
+				}
+				System.out.println("\t</" + nNode.getNodeName() + ">");
+			}
+			System.out.println("\n</" + doc.getDocumentElement().getNodeName() + ">");
+
+			
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer tformer = tFactory.newTransformer();
+            tformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            StreamResult consoleResult = new StreamResult(System.out);
+            tformer.transform(source, consoleResult); 
+
+            File OutPutFile = new File("DomReadQTFL19.xml");
+            OutputStream outputStream = new FileOutputStream(OutPutFile);
+            StreamResult fileResult = new StreamResult(outputStream);
+            tformer.transform(source, fileResult);
+            outputStream.close();
+
+    
+		}catch (ParserConfigurationException | IOException | TransformerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 }
